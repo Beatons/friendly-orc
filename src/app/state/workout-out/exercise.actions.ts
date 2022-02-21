29376fe5936c35decdeb1f91795e85431a92b9ca
Nerijus Pamedytis <studentas.nerijus@gmail.com>
2022-02-21 21:14:09 +0100

    commit before rewrite into date based workout

---
+++ src/app/state/workout-out/exercise.actions.ts
@@ -0,0 +1,15 @@
+export class StartExercise {
+  static readonly type = '[Workout] StartExercise';
+}
+
+export class PauseExercise {
+  static readonly type = '[Workout] PauseExercise';
+}
+
+export class EndWorkout {
+  static readonly type = '[Workout] EndWorkout';
+}
+
+export class NextExercise {
+  static readonly type = '[Workout] NextExercise';
+}
